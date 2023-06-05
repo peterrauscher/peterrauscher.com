21afc08df293f7b3854394fbe4c487f3df9d304a
Peter Rauscher <peterrauscher@protonmail.com>
2023-06-04 20:57:38 -0400

    Added logic for mobile dropdown button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  isActive = false;
+  navbarToggle() {
+    this.isActive = !this.isActive;
+  }
 }
