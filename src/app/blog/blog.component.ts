1471c35e90677dddfaaa6198d554470867c887c7
Peter Rauscher <peterrauscher@protonmail.com>
2023-06-10 22:37:50 -0400

    Blog page lists posts, still need to add actual post page components

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import posts from 'src/posts.json';
+
+interface Post {
+  title: string;
+  date: string;
+  content: string;
+  thumbnail: string;
+}
+
+let postData = posts.map((p) => {
+  const post: Post = {
+    title: p.data.title,
+    date: new Date(p.data.date).toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    }),
+    content: p.content,
+    thumbnail: `background-image: url("/assets/thumbnails/${p.data.thumbnail}"); background-position: center; background-size: cover;`,
+  };
+  return post;
+});
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
 })
-export class BlogComponent {}
+export class BlogComponent {
+  posts = postData;
+}
